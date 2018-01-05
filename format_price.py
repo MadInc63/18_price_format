e55e157c874ec79e63f4529340f9ef23cabccdf8
MadInc63 <MadInc63@gmail.com>
2018-01-05 14:27:53 +0300

    build function build unittest

--- format_price.py
+++ format_price.py
@@ -1,5 +1,29 @@
+import argparse
+import string
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Price formatting')
+    parser.add_argument(
+        'price',
+        type=str,
+        help='enter price')
+    return parser.parse_args()
+
+
 def format_price(price):
-    pass
+    price_characters = set(price)
+    letters = set(string.ascii_letters)
+    punctuation = set(string.punctuation)
+    punctuation.remove('.')
+    whitespace = set(string.whitespace)
+    for pattern in (letters, punctuation, whitespace):
+        if pattern & price_characters or tuple(price).count('.') >= 2:
+            return 'Invalid date type. Enter price in float or int.'
+    return '{:,.2f}'.format(float(price)).replace(',', ' ').replace('.00', '')
+
 
 if __name__ == '__main__':
-    pass
+    args = parse_args()
+    formatted_price = format_price(args.price)
+    print(formatted_price)
--- tests.py
+++ tests.py
@@ -1,1 +1,63 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_integer(self):
+        price = format_price('123456789')
+        self.assertEqual(price, '123 456 789')
+
+    def test_float_round_up(self):
+        price = format_price('123456.789')
+        self.assertEqual(price, '123 456.79')
+
+    def test_float_round_down(self):
+        price = format_price('123456.123')
+        self.assertEqual(price, '123 456.12')
+
+    def test_without_integer_before_dot(self):
+        price = format_price('.123')
+        self.assertEqual(price, '0.12')
+
+    def test_without_integer_after_dot(self):
+        price = format_price('123.')
+        self.assertEqual(price, '123')
+
+    def test_letters(self):
+        price = format_price('abcdefg')
+        self.assertEqual(
+            price,
+            'Invalid date type. Enter price in float or int.'
+        )
+
+    def test_symbol(self):
+        price = format_price('#$%^&*')
+        self.assertEqual(
+            price,
+            'Invalid date type. Enter price in float or int.'
+        )
+
+    def test_punctuation(self):
+        price = format_price('123456,789')
+        self.assertEqual(
+            price,
+            'Invalid date type. Enter price in float or int.'
+        )
+
+    def test_punctuation_multiple_dot(self):
+        price = format_price('123.456.789')
+        self.assertEqual(
+            price,
+            'Invalid date type. Enter price in float or int.'
+        )
+
+    def test_whitespace(self):
+        price = format_price('123 456789')
+        self.assertEqual(
+            price,
+            'Invalid date type. Enter price in float or int.'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
